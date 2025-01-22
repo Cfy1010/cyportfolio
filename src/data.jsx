dfba6704344569e8f7157d838000a6db3931c631
Cfy1010 <cy_one2000@yahoo.fr>
2025-01-22 17:57:48 +0100

    Add skills

--- src/data.jsx
+++ src/data.jsx
@@ -1,11 +1,11 @@
 import { nanoid } from "nanoid";
-import { FaHtml5, FaJs, FaReact } from "react-icons/fa";
+import { FaHtml5, FaCss3, FaJs, FaReact } from "react-icons/fa";
 
 export const links = [
-  { id: nanoid(), href: "#home", text: "Accueil " },
-  { id: nanoid(), href: "#about", text: "à propos" },
-  { id: nanoid(), href: "#skills", text: "compétences" },
-  { id: nanoid(), href: "#projects", text: "projets" },
+  { id: nanoid(), href: "#home", text: "home" },
+  { id: nanoid(), href: "#skills", text: "skills" },
+  { id: nanoid(), href: "#about", text: "about" },
+  { id: nanoid(), href: "#projects", text: "projects" },
 ];
 
 export const skills = [
@@ -13,19 +13,19 @@ export const skills = [
     id: nanoid(),
     title: "HTML&CSS",
     icon: <FaHtml5 className="h-16 w-16 text-emerald-500" />,
-    text: "Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.",
+    text: "Expert en HTML et CSS, capable de créer des sites web visuellement attractifs et responsives pour offrir une expérience utilisateur optimale.",
   },
   {
     id: nanoid(),
     title: "Javascript",
     icon: <FaJs className="h-16 w-16 text-emerald-500" />,
-    text: "Expertise in JavaScript, building interactive and dynamic web applications with a focus on seamless user interactions and functionality",
+    text: "Compétence en JavaScript, pour la création d'applications web interactives et dynamiques, en mettant l'accent sur des interactions utilisateur fluides et une fonctionnalité optimale.",
   },
   {
     id: nanoid(),
     title: "React",
     icon: <FaReact className="h-16 w-16 text-emerald-500" />,
-    text: "Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.",
+    text: "Maîtrise avancée de React, pour le développement d'applications front-end efficaces et interactives, avec un fort accent sur l'architecture basée sur les composants.",
   },
 ];
 
